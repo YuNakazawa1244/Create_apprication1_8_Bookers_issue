a8b2a9050faa5e78cf9c2937666e1f2708a04972
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-14 14:16:27 +0000

    [Finish]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  get '/', to: 'homes#top'
+  root to: 'homes#top'
+  # get '/', to: 'homes#top'
 
   post 'books', to: 'books#create'
 

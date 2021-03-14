a9da47df226ed8e347a221d9919c3566f273ac4a
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-14 13:34:38 +0000

    [Add]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -13,16 +13,24 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book)
+       flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def create
-     book = Book.new(book_params)
-     book.save
-     redirect_to book_path(book.id)
-     # redirect_to books_path(book.id)
+     @book = Book.new(book_params)
+     if @book.save
+        redirect_to book_path(@book.id)
+        flash[:notice] = "Book was successfully created."
+     else
+        @books = Book.all
+        render :index
+     end
   end
 
   def destroy
